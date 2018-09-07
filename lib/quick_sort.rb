d7a67bd38756b79ef7863697cba17b51c135de23
Jordan Yu <jordanyu44@gmail.com>
2018-09-06 21:29:46 -0700

    Build quicksort not in-place

--- lib/quick_sort.rb
+++ lib/quick_sort.rb
@@ -4,6 +4,20 @@ class QuickSort
 
   # Not in-place. Uses O(n) memory.
   def self.sort1(array)
+    return array if array.length <= 1
+    left, right, mid = [], [], []
+    pivot = array[rand(array.length)]
+    array.each do |el|
+      case el <=> pivot
+      when -1
+        left << el
+      when 0
+        mid << el 
+      else 
+        right << el
+      end
+    end
+    QuickSort.sort1(left) + mid + QuickSort.sort1(right)
   end
 
   # In-place.
